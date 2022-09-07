30ffa8fa0b347518e7ddd5807f13bf4fd0438071
Luciano Rocha <luciano@lucianorocha.dev>
2022-09-07 10:16:20 -0300

    Create function to randomly return 'Rock', 'Paper' or 'Scissors'

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+// Randomly return either 'Rock', 'Paper' or 'Scissors'
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3) + 1;
+  if (randomNumber === 1) {
+    let computerChoice = "Rock";
+    return computerChoice;
+  } else if (randomNumber === 2) {
+    let computerChoice = "Paper";
+    return computerChoice;
+  } else {
+    let computerChoice = "Scissors";
+    return computerChoice;
+  }
+}
 
+console.log(getComputerChoice());
